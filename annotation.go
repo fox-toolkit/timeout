757e2c7a790b70b6c94ceedb54787b155ce1c8d0
Sylvain Muller <sylvainmuller@protonmail.com>
2026-02-01 22:35:39 +0100

    refactor: use generic function for route timeout unwrapping and add r… (#17)
    * refactor: use generic function for route timeout unwrapping and add repository transfer notice
    
    * docs: update codecov badge token for fox-toolkit org

--- annotation.go
+++ annotation.go
@@ -10,12 +10,10 @@ import (
 	"github.com/fox-toolkit/fox"
 )
 
-type key struct{}
-
-var (
-	timeoutKey      key
-	readTimeoutKey  key
-	writeTimeoutKey key
+type (
+	hKey struct{}
+	rKey struct{}
+	wKey struct{}
 )
 
 const NoTimeout = time.Duration(0)
@@ -24,24 +22,22 @@ const NoTimeout = time.Duration(0)
 // This allows individual routes to have different timeout values than the global timeout.
 // Passing a value <= 0 (or NoTimeout) disables the timeout for this route.
 func OverrideHandler(dt time.Duration) fox.RouteOption {
-	return fox.WithAnnotation(timeoutKey, dt)
+	return fox.WithAnnotation(hKey{}, dt)
 }
 
 // OverrideRead returns a RouteOption that sets the read deadline for the underlying connection.
-// This controls how long the server will wait for the client to send request data.
-// A zero duration is not allowed and will return an error during route registration.
+// This controls how long the server will wait before timing out while reading the request body.
 func OverrideRead(dt time.Duration) fox.RouteOption {
-	return fox.WithAnnotation(readTimeoutKey, dt)
+	return fox.WithAnnotation(rKey{}, dt)
 }
 
 // OverrideWrite returns a RouteOption that sets the write deadline for the underlying connection.
 // This controls how long the server will wait before timing out writes to the client.
-// A zero duration is not allowed and will return an error during route registration.
 func OverrideWrite(dt time.Duration) fox.RouteOption {
-	return fox.WithAnnotation(writeTimeoutKey, dt)
+	return fox.WithAnnotation(wKey{}, dt)
 }
 
-func unwrapRouteTimeout(r *fox.Route, k key) (time.Duration, bool) {
+func unwrapRouteTimeout[T comparable](r *fox.Route, k T) (time.Duration, bool) {
 	if r != nil {
 		dt := r.Annotation(k)
 		if dt != nil {
